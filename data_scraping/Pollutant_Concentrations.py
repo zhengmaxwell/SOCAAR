0ec95102b04b1a09e69aaeee7bfa3d26e75544a5
Maxwell <zhengmaxwell@gmail.com>
2021-05-15 19:42:35 -0400

    functions to check valid time range and insert data

--- data_scraping/Pollutant_Concentrations.py
+++ data_scraping/Pollutant_Concentrations.py
@@ -1,17 +1,94 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import psycopg2
-from typing import Dict
+from datetime import timedelta, datetime
+import sys
+from typing import Dict, Union
+
+
 
 class Pollutant_Concentrations():
 
-    def __init__(self, host: str, database: str, user: str, password: str):
+    def __init__(self, host: str, database: str, user: str, password: str, table: str) -> None:
 
         self.host = host
         self.database = database
         self.user = user
         self.password = password
         self.conn = None
+        self.table = table
+
+        self._connect()
+
+
+    def create_table(self) -> None:
+
+        command = "SELECT * FROM pg_catalog.pg_tables"
+        rows = self.command(command, 'r')
+ 
+        if self.table not in [row[1] for row in rows]: # only create if does not exist yet
+
+            command = f"""
+                CREATE TABLE {self.table}(
+                    id SERIAL PRIMARY KEY,
+                    added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                    year INTEGER NOT NULL CHECK (year <= DATE_PART('year', now())),
+                    month INTEGER NOT NULL CHECK (month >= 1 and month <= 12),
+                    day INTEGER NOT NULL CHECK (day >= 1 and day <= 31),
+                    hour INTEGER NOT NULL CHECK (hour >= 0 and hour <= 23),
+                    city VARCHAR NOT NULL,
+                    o3 FLOAT,
+                    pm2_b FLOAT,
+                    no2 FLOAT,
+                    so2 FLOAT,
+                    co FLOAT
+                )
+            """
+
+            self.command(command)
+
+
+    def command(self, command: str, mode: str) -> Union[None, str]:
+
+        if mode not in ('r', 'w'): # read and write
+            print("ERROR: mode parameter must be 'r' or 'w'")
+            sys.exit()
+
+        cur = self.conn.cursor()
+
+        try:
+            cur.execute(command)
+        except Exception as e:
+            print(e)
+            sys.exit()
+        
+        if mode == 'r':
+            rows = cur.fetchall()
+            cur.close()
+            
+            return rows
+
+        else:
+            cur.close()
+            self.conn.commit()
+
+
+    def insert_data(self, year: int, month: int, day: int, hour: int) -> None:
+
+        command = f"SELECT COUNT(*) FROM {self.table}"
+        count = self.command(command, 'r')[0][0]
+
+        if count == 0: # check if data already exists
+            data = self._get_data(year, month, day, hour)
+
+            for city in data:
+                command = f"""
+                    INSERT INTO {self.table} (year, month, day, hour, city, o3, pm2_5, no2, so2, co) 
+                    VALUES ({year}, {month}, {day}, {hour}, '{city}', {data[city]["O3"]}, {data[city]["PM2.5"]}, {data[city]["NO2"]}, {data[city]["SO2"]}, {data[city]["CO"]})
+                """
+
+                self.command(command, 'w')
+
 
     def _connect(self) -> None:
 
@@ -25,38 +102,25 @@ class Pollutant_Concentrations():
 
             self.conn = conn
 
-        except:
-            raise Exception("Failed to connect")
-
-    def create_table(self) -> None:
-
-        if not self.conn:
-            self._connect()
-
-        command = """
-            CREATE TABLE socaar.POLLUTANT_CONCENTRATIONS(
-                id SERIAL PRIMARY KEY,
-                added_at TIMESTAMP WITH TIME ZONE DEFAULT_TIMESTAMP,
-                year INTEGER NOT NULL CHECK (year <= DATE_PART('year', now())),
-                month INTEGER NOT NULL CHECK (month >= 1 and month <= 12),
-                day INTEGER NOT NULL CHECK (day >= 1 and day <= 31),
-                hour INTEGER NOT NULL CHECK (hour >= 0 and hour <= 23),
-                city VARCHAR NOT NULL,
-                o3 FLOAT,
-                pm2_b FLOAT,
-                no2 FLOAT,
-                so2 FLOAT,
-                co FLOAT
-            )
-        """
-
-        cur = self.conn.cursor()
-        cur.execute(command)
-        cur.close()
-        conn.commit()
+        except Exception as e:
+            print(e)
+            sys.exit()
 
 
-    def _get_data(self, day: int, month: int, year: int, hour: int) -> Dict[str, Dict[str, float]]:
+    def _get_valid_time_range(self) -> List[datetime]:
+        
+        command = f"SELECT * FROM {self.table} ORDER BY year DESC, month DESC, day DESC, hour DESC LIMIT 1"
+        row = self.command(command, 'r')
+        most_recent = datetime(row[0][2], row[0][3], row[0][4], row[0][5]) if row else datetime.now() # returns current datetime if no entries yet
+        
+        now = datetime.now()
+        hours = int(divmod((now - most_recent).total_seconds(), 3600)[0])
+        time_range = [most_recent + timedelta(hours=i) for i in range(1, hours+1)]
+
+        return time_range
+
+
+    def _get_data(self, year: int, month: int, day: int, hour: int) -> Dict[str, Dict[str, float]]:
 
         data = {}
         column_order = {}
@@ -86,15 +150,6 @@ class Pollutant_Concentrations():
             data[city] = {}
 
             for col in range(1, len(td)):
-                data[city][column_order[col]] = td[col].div.text if td[col].div else None
+                data[city][column_order[col]] = 0 if (not td[col].div or not td[col].div.text.isnumeric()) else td[col].div.text
 
         return data
-
-
-
-    
-
-
-
-
-    
