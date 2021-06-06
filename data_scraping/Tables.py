00f3e0c8031716a0d2be39359aedd82cdd393277
Maxwell <zhengmaxwell@gmail.com>
2021-06-06 14:54:59 -0400

    added validation codes table

--- data_scraping/Tables.py
+++ data_scraping/Tables.py
@@ -4,11 +4,14 @@ import json
 
 class Tables:
 
-    MOE = "moe_pollutant_concentrations"
+    
     MOE_STATIONS = "moe_stations"
     NAPS_STATIONS = "naps_stations"
+    NAPS_VALIDATION_CODES = "naps_validation_codes"
+    MOE = "moe_pollutant_concentrations"
     NAPS_CONTINUOUS = "naps_continuous_pollutant_concentrations"
     NAPS_INTEGRATED_CARBONYLYS = "naps_integrated_carbonyls_pollutant_concentrations"
+
 
     @staticmethod
     def create_moe_stations(psql: Postgres) -> None:
@@ -60,6 +63,31 @@ class Tables:
                     VALUES ({row["NAPS_ID"]}, %(name)s, {row["Latitude"] or "Null"}, {row["Longitude"] or "Null"})
                 """
                 str_params = {"name": row["Station_Name"]}
+                psql.command(command, 'w', str_params=str_params)
+
+    @staticmethod
+    def create_naps_validation_codes(psql: Postgres) -> None:
+
+        if not psql.does_table_exist(Tables.NAPS_VALIDATION_CODES):
+            command = f"""
+                CREATE TABLE {Tables.NAPS_VALIDATION_CODES} (
+                    id SERIAL PRIMARY KEY,
+                    validation_code CHAR(2),
+                    description VARCHAR NOT NULL
+                )
+            """
+            psql.command(command, 'w')
+
+            with open(f"{os.path.dirname(__file__)}/naps_data/validation_codes.json", 'r') as validation_codes_file:
+                data = json.loads(validation_codes_file.read())
+
+            for row in data:
+                validation_code = "%(validation_code)s" if row["Validation Code"] else "Null"
+                command = f"""
+                    INSERT INTO {Tables.NAPS_VALIDATION_CODES} (validation_code, description)
+                    VALUES ({validation_code}, %(description)s)
+                """
+                str_params = {"validation_code": row["Validation Code"], "description": row["Description"]}
                 psql.command(command, 'w', str_params=str_params)
 
     @staticmethod
